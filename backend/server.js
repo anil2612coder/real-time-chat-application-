153a8c0edfb4d8f428b99f80dba38fa7362dd01c
Anil <ak26122k@gmail.com>
2024-02-23 13:13:06 +0530

    create api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import  express from "express";
+import dotenv from "dotenv"
+import authRouthes from "./routes/authRouthes.js"
+
 
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, ()=>{
-    console.log("server is running")
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+app.use("/api/auth", authRouthes)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
 })
