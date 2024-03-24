a253e9331156d8ef021c543aeb9f0f33ae4c9219
Anil <ak26122k@gmail.com>
2024-03-24 07:05:26 +0530

    complete code

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import  express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/authRoutes.js"
@@ -8,12 +9,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app ,server} from "./socket/socket.js";
 
-
+const __dirname = path.resolve();
 
 
 dotenv.config()
 const PORT = process.env.PORT || 5000
-
 
 
 connectToMongodb()
@@ -23,6 +23,12 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 app.use("/api/users", userRoutes)
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
