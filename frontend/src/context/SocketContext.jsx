ff2a9a9fe761e952484dcbd5f18e21ce688d2dc7
Anil <ak26122k@gmail.com>
2024-03-24 09:39:30 +0530

    some changes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://real-time-chat-website.onrender.com/", {
+      const socket = io("https://real-time-chat-website.onrender.com", {
         query: {
           userId: authUser._id,
         },
